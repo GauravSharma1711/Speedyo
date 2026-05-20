f62db1f774b3a76fc65025c84130cc1e06bda9d9
Gaurav Sharma <gau1711sha@gmail.com>
2026-05-20 12:21:56 +0530

    updated next auth for production

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,15 @@ import { NextRequest, NextResponse } from "next/server";
 import { getToken } from "next-auth/jwt"
 
 export async function middleware(request:NextRequest) {
-    const token = await getToken({ req: request, secret: process.env.NEXTAUTH_SECRET })
+
+      const token = await getToken({ 
+    req: request, 
+    secret: process.env.NEXTAUTH_SECRET,
+    cookieName: process.env.NODE_ENV === "production" 
+      ? "__Secure-next-auth.session-token" 
+      : "next-auth.session-token",
+  });
+  
     const url = request.nextUrl
 
     if(token &&  (
